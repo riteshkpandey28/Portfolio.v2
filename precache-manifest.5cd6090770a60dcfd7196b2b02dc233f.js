55073ff10e9cd75d1c19e2626cf1196cce11ecc9
stupiloid-coder <riteshkumar.dp@somaiya.edu>
2022-08-08 00:45:11 +0530

    Updates

---
+++ precache-manifest.5cd6090770a60dcfd7196b2b02dc233f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "075ea447bfa6137b74aa",
+    "url": "/portfoliov2/static/js/main.b4c21177.chunk.js"
+  },
+  {
+    "revision": "d26f7f1865ac6d5c3c1f",
+    "url": "/portfoliov2/static/js/runtime~main.20d58baa.js"
+  },
+  {
+    "revision": "f40e1c737c24c6dfa994",
+    "url": "/portfoliov2/static/css/2.5227f1fb.chunk.css"
+  },
+  {
+    "revision": "f40e1c737c24c6dfa994",
+    "url": "/portfoliov2/static/js/2.f91b8d5a.chunk.js"
+  },
+  {
+    "revision": "6c3e9ce5f89d0b6ce1427935898123be",
+    "url": "/portfoliov2/static/media/resume.6c3e9ce5.pdf"
+  },
+  {
+    "revision": "cb89bb7bc06037f7088e6804bd35b8d7",
+    "url": "/portfoliov2/static/media/bg-video.cb89bb7b.mp4"
+  },
+  {
+    "revision": "17f2f3dfe2806d3fdfee183fc806f5e1",
+    "url": "/portfoliov2/static/media/profile.17f2f3df.jpg"
+  },
+  {
+    "revision": "705c4dfeb6936a96c003a79604483885",
+    "url": "/portfoliov2/index.html"
+  }
+];
